e717b2340ac981f8cdf24955b9b6c5a459bd599f
micopes <copes.km@gmail.com>
2023-02-12 21:29:36 +0900

    Spring Bean Using Code

---
+++ hello-spring/src/main/java/com/example/demo/SpringConfig.java
@@ -0,0 +1,24 @@
+package com.example.demo;
+
+import com.example.demo.repository.MemberRepository;
+import com.example.demo.repository.MemoryMemberRepository;
+import com.example.demo.service.MemberService;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SpringConfig {
+
+    // Spring이 올라올 때
+    // 1. memberService를 올린다.
+    // 2. memberRepository를 올린다.
+    @Bean
+    public MemberService memberService() {
+        return new MemberService(memberRepository());
+    }
+
+    @Bean
+    public MemberRepository memberRepository() {
+        return new MemoryMemberRepository();
+    }
+}
