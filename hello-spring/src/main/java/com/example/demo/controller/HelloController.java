ad7e674b02cf50e658acfa8c433074b48cbaa647
micopes <copes.km@gmail.com>
2023-02-12 15:57:05 +0900

    MVC / Template engine

--- hello-spring/src/main/java/com/example/demo/controller/HelloController.java
+++ hello-spring/src/main/java/com/example/demo/controller/HelloController.java
@@ -3,6 +3,7 @@ package com.example.demo.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HelloController {
@@ -15,5 +16,12 @@ public class HelloController {
         // 리턴값 "hello"는 hello.html의 파일 이름 "hello"를 말하는 것 . 이곳으로 가서 렌더링.
         // 컨트롤러에서 리턴 값으로 문자를 반환하면 viewResolver가 화면을 찾아서 처리한다.
         return "hello";
+    }
+
+    // @RequestParam이 default가 값이 있어야 하는 것이므로 인자로 "?....." 을 넣어줘야 한다. ex) localhost:8080/hello-mvc?name=spring!!!
+    @GetMapping("hello-mvc")
+    public String helloMvc(@RequestParam("name") String name, Model model) {
+        model.addAttribute("name", name);
+        return "hello-template";
     }
 }
