ff015ec00a77e4aa0e8088a560988b143324b660
micopes <copes.km@gmail.com>
2023-02-12 16:45:27 +0900

    API

--- hello-spring/src/main/java/com/example/demo/controller/HelloController.java
+++ hello-spring/src/main/java/com/example/demo/controller/HelloController.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
@@ -23,5 +24,35 @@ public class HelloController {
     public String helloMvc(@RequestParam("name") String name, Model model) {
         model.addAttribute("name", name);
         return "hello-template";
+    }
+
+    @GetMapping("hello-string")
+    @ResponseBody // Body 부분에 리턴 값에 해당하는 내용을 그대로 넣어주겠다는 것. View 같은게 없음
+    public String helloString(@RequestParam("name") String name) {
+        return "hello " + name;
+    }
+
+    // 객체(Hello)를 넘기면 JsonConverter
+    // 문자를 넘기면 StringConverter
+    // 두 경우 모두 - JSON 형태로 반환 (Default. XML로 반환할 수도 있긴 함)
+    @GetMapping("hello-api")
+    @ResponseBody
+    public Hello helloApi(@RequestParam("name") String name) {
+        Hello hello = new Hello();
+        hello.setName(name);
+        return hello;
+    }
+
+    static class Hello {
+        private String name;
+
+        // Getter & Setter. private를 getter/setter 메소드를 통해 가져올 수 있도록 함 - 프로퍼티 접근 방식.
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
     }
 }
