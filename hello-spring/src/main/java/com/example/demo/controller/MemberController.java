48200f8c30da8adb9f3750070dc12926bd5cb062
micopes <copes.km@gmail.com>
2023-02-13 22:24:27 +0900

    new member

--- hello-spring/src/main/java/com/example/demo/controller/MemberController.java
+++ hello-spring/src/main/java/com/example/demo/controller/MemberController.java
@@ -1,8 +1,11 @@
 package com.example.demo.controller;
 
+import com.example.demo.domain.Member;
 import com.example.demo.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 // @Controller -> 스프링 컨테이너에 컨트롤러가 생성 및 관리된다.
 @Controller
@@ -14,4 +17,18 @@ public class MemberController {
         this.memberService = memberService;
     }
 
+    @GetMapping("/members/new")
+    public String createForm() {
+        return "members/createMemberForm"; // templates에서 찾음. (html이 뿌려지는 것)
+    }
+
+    @PostMapping("/members/new")
+    public String create(MemberForm form) {
+        Member member = new Member();
+        member.setName(form.getName()); // html form tag에 있는 값을 가져와서 member에 넣어준다.
+
+        memberService.join(member);
+
+        return "redirect:/";
+    }
 }
