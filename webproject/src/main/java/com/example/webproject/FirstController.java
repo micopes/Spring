b77c987010174c749f34e07aee396316c8bfc4b6
micopes <copes.km@gmail.com>
2023-03-05 19:11:04 +0900

    MVC, View template layout

---
+++ webproject/src/main/java/com/example/webproject/FirstController.java
@@ -0,0 +1,24 @@
+package com.example.webproject;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class FirstController {
+
+    @GetMapping("/hi")
+    public String niceToMeetYou(Model model) {
+        model.addAttribute("username", "Minjin");
+
+        return "greetings";
+    }
+
+    @GetMapping("/bye")
+    public String seeYouLater(Model model) {
+        model.addAttribute("nickname", "Gildong");
+
+        return "goodbye";
+    }
+}
