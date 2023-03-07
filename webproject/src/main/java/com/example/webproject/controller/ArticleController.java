bd5c337ec8f69994817e123f44ccc0e900bad445
micopes <copes.km@gmail.com>
2023-03-08 01:39:16 +0900

    update form data to DB

--- webproject/src/main/java/com/example/webproject/controller/ArticleController.java
+++ webproject/src/main/java/com/example/webproject/controller/ArticleController.java
@@ -69,4 +69,39 @@ public class ArticleController {
         // 3. View 페이지를 설정
         return "articles/index";
     }
+
+    @GetMapping("/articles/{id}/edit")
+    public String edit(@PathVariable Long id, Model model) {
+
+        // 수정할 데이터를 가져오기
+        Article articleEntity = articleRepository.findById(id).orElse(null);
+
+        // 모델에 데이터를 등록
+        model.addAttribute("article", articleEntity);
+
+        // View Page 설정
+        return "articles/edit";
+    }
+
+    @PostMapping("/articles/update")
+    public String update(ArticleForm form) {
+        log.info(form.toString());
+
+        // 1. DTO -> Entity
+        Article articleEntity = form.toEntity();
+        log.info(articleEntity.toString());
+
+        // 2. 엔티티를 DB로 저장한다.
+        // 2-1: DB에서 기존 데이터를 가져온다.
+        Article target = articleRepository.findById(articleEntity.getId()).orElse(null);
+
+
+        // 2-2: 기존 데이터가 있다면 값을 갱신한다.
+        if (target != null) {
+            articleRepository.save(articleEntity); // Entity가 DB로 갱신
+        }
+
+        // 3. 수정 결과 페이지로 리다이렉트
+        return "redirect:/articles/" + articleEntity.getId();
+    }
 }
--- webproject/src/main/java/com/example/webproject/dto/ArticleForm.java
+++ webproject/src/main/java/com/example/webproject/dto/ArticleForm.java
@@ -8,12 +8,14 @@ import lombok.ToString;
 @ToString // toString() 대체
 public class ArticleForm {
 
+    private Long id;
     private String title;
     private String content;
 
 
 
     public Article toEntity() {
-        return new Article(null, title, content);
+
+        return new Article(id, title, content);
     }
 }
